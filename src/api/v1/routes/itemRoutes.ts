273f9acd2884df6754923888ef600af9162e1f56
Christian Bugtai <69916073+ChristianBugtai@users.noreply.github.com>
2025-03-31 03:32:33 +0000

    updated itemRoutes to match the updated itemController

--- src/api/v1/routes/itemRoutes.ts
+++ src/api/v1/routes/itemRoutes.ts
@@ -5,10 +5,10 @@ import * as itemController from "../controllers/itemController"
 const router: Router = express.Router();
 
 // "/api/v1/items"
-router.get("/", itemController.getItems);
-router.get("/:id", itemController.getItem);
-router.post("/", itemController.addItem);
-router.put("/:id", itemController.updateItem);
-router.delete("/:id", itemController.deleteItem);
+router.get("/:id", itemController.getDocumentById);
+router.get("/", itemController.getDocuments);
+router.post("/", itemController.addDocument);
+router.put("/:id", itemController.updateDocument);
+router.delete("/:id", itemController.deleteDocument);
 
 export default router;
