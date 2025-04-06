5fc82eb9ca6389f5ebe64f9203071364e37f61f3
Christian Bugtai <69916073+ChristianBugtai@users.noreply.github.com>
2025-04-06 20:21:53 +0000

    testing for other routes

---
+++ test/locationRoutes.test.ts
@@ -0,0 +1,76 @@
+import request from "supertest";
+import { Request, Response, NextFunction } from "express";
+
+import app from "../src/app"
+import { 
+    getEntryById, 
+    getEntries, 
+    addEntry, 
+    updateEntry, 
+    deleteEntry 
+} from "../src/api/v1/controllers/entryController"; 
+
+beforeAll(() => {
+    const mockGetEntryById = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockGetEntries = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockAddEntry = jest.fn((req: Request, res: Response) => res.status(201).send());
+    const mockUpdateEntry = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockDeleteEntry = jest.fn((req: Request, res: Response) => res.status(200).send());
+
+    jest.mock("../src/api/v1/controllers/entryController", () => ({
+        getEntryById: () => mockGetEntryById,
+        getEntries: () => mockGetEntries,
+        addEntry: () => mockAddEntry,
+        updateEntry: () => mockUpdateEntry,
+        deleteEntry: () => mockDeleteEntry,
+    }));
+})
+
+describe("Location Routes", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("GET /api/v1/locations/:id", () => {
+        it("should call the getEntryById controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).get(`/api/v1/locations/${mockId}`)
+            expect(getEntryById).toHaveBeenCalled
+        })
+    })
+
+    describe("GET /api/v1/locations/", () => {
+        it("should call the getEntries controller", async () => {
+
+            await request(app).get(`/api/v1/locations/}`)
+            expect(getEntries).toHaveBeenCalled
+        })
+    })
+
+    describe("POST /api/v1/locations/", () => {
+        it("should call the addEntry controller", async () => {
+
+            await request(app).post(`/api/v1/locations/}`)
+            expect(addEntry).toHaveBeenCalled
+        })
+    })
+
+    describe("PUT /api/v1/locations/:id", () => {
+        it("should call the updateEntry controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).put(`/api/v1/locations/${mockId}}`)
+            expect(updateEntry).toHaveBeenCalled
+        })
+    })
+
+    describe("DELETE /api/v1/locations/:id", () => {
+        it("should call the updateEntry controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).delete(`/api/v1/locations/${mockId}}`)
+            expect(deleteEntry).toHaveBeenCalled
+        })
+    })
+});
---
+++ test/treasureRoutes.test.ts
@@ -0,0 +1,76 @@
+import request from "supertest";
+import { Request, Response, NextFunction } from "express";
+
+import app from "../src/app"
+import { 
+    getEntryById, 
+    getEntries, 
+    addEntry, 
+    updateEntry, 
+    deleteEntry 
+} from "../src/api/v1/controllers/entryController"; 
+
+beforeAll(() => {
+    const mockGetEntryById = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockGetEntries = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockAddEntry = jest.fn((req: Request, res: Response) => res.status(201).send());
+    const mockUpdateEntry = jest.fn((req: Request, res: Response) => res.status(200).send());
+    const mockDeleteEntry = jest.fn((req: Request, res: Response) => res.status(200).send());
+
+    jest.mock("../src/api/v1/controllers/entryController", () => ({
+        getEntryById: () => mockGetEntryById,
+        getEntries: () => mockGetEntries,
+        addEntry: () => mockAddEntry,
+        updateEntry: () => mockUpdateEntry,
+        deleteEntry: () => mockDeleteEntry,
+    }));
+})
+
+describe("Treasure Routes", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe("GET /api/v1/treasures/:id", () => {
+        it("should call the getEntryById controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).get(`/api/v1/treasures/${mockId}`)
+            expect(getEntryById).toHaveBeenCalled
+        })
+    })
+
+    describe("GET /api/v1/treasures/", () => {
+        it("should call the getEntries controller", async () => {
+
+            await request(app).get(`/api/v1/treasures/}`)
+            expect(getEntries).toHaveBeenCalled
+        })
+    })
+
+    describe("POST /api/v1/treasures/", () => {
+        it("should call the addEntry controller", async () => {
+
+            await request(app).post(`/api/v1/treasures/}`)
+            expect(addEntry).toHaveBeenCalled
+        })
+    })
+
+    describe("PUT /api/v1/treasures/:id", () => {
+        it("should call the updateEntry controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).put(`/api/v1/treasures/${mockId}}`)
+            expect(updateEntry).toHaveBeenCalled
+        })
+    })
+
+    describe("DELETE /api/v1/treasures/:id", () => {
+        it("should call the updateEntry controller", async () => {
+            const mockId: string = "101";
+
+            await request(app).delete(`/api/v1/treasures/${mockId}}`)
+            expect(deleteEntry).toHaveBeenCalled
+        })
+    })
+});
